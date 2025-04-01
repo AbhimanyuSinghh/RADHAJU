e7ca366c5174a18a2b5dfa06be01b9b879349230
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2025-04-02 01:49:06 +0530

    first commit

---
+++ radheradhe.java
@@ -0,0 +1,6 @@
+class radheradhe {
+    public static void main(String[] args) {
+        System.out.println("Radhe Radhe!"); // Print "Radhe Radhe!" to the console
+        System.out.println("Jai Shree Krishna!"); // Print "Jai Shree Krishna!" to the console
+    }
+}
