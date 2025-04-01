106c23a99084c7882228efa161dee81d15d4356b
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2025-04-02 02:13:06 +0530

    second commit

--- radheradhe.java
+++ radheradhe.java
@@ -2,5 +2,7 @@ class radheradhe {
     public static void main(String[] args) {
         System.out.println("Radhe Radhe!"); // Print "Radhe Radhe!" to the console
         System.out.println("Jai Shree Krishna!"); // Print "Jai Shree Krishna!" to the console
+        System.out.println("Hare Krishna!"); // Print "Hare Krishna!" to the console
+        System.out.println("Hare Ram!"); // Print "Hare Ram!" to the console
     }
 }
